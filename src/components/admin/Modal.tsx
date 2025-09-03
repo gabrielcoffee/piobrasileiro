c0a7544749f1a25d637f20416c1439d86af4e9e1
gabrielcoffee <cafegabriel1@gmail.com>
2025-09-03 12:27:59 -0300

    fix: modal overlay onClose function being activated when clicking on the modal itself (bubbling up)

--- src/components/admin/Modal.tsx
+++ src/components/admin/Modal.tsx
@@ -19,7 +19,7 @@ export default function Modal({ children, buttons, title, subtitle, onClose, isO
     return (
         <div className={styles.overlay} onClick={onClose}>
 
-            <div className={styles.modal}>
+            <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
 
                 <div className={styles.header}>
 
