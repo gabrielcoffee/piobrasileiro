5d907ae463284a26b43c952036603f5921db81fa
gabrielcoffee <cafegabriel1@gmail.com>
2025-09-08 23:21:37 -0300

    feat: admin can access details on requests

--- src/app/admin/(admin-protected)/(hospedagem)/solicitacoes/page.tsx
+++ src/app/admin/(admin-protected)/(hospedagem)/solicitacoes/page.tsx
@@ -8,18 +8,30 @@ import Table from '@/components/admin/Table';
 import { getDateString, queryApi } from '@/lib/utils';
 import { Button } from '@/components/ui/Button';
 import { Eye } from 'lucide-react';
+import Modal from '@/components/admin/Modal';
 
 export default function SolicitacoesPage() {
 
     const [solicitacoes, setSolicitacoes] = useState<any[]>([]);
+    const [isDetalhesModalOpen, setIsDetalhesModalOpen] = useState<boolean>(false);
+    const [selectedSolicitacao, setSelectedSolicitacao] = useState<any>(null);
 
-    const acoes = (id: string) => {
+    const acoes = (request: any) => {
         return (
             <div className={styles.acoes}>
-                <Button variant="full-white" iconLeft={<img src="/wpp.svg" width={20} alt="Whatsapp" />}>Contatar</Button>
-                <Button variant="full-white" iconLeft={<Eye size={20} />}>Detalhes</Button>
+                <Button onClick={() => handleContatar(request.num_telefone)} variant="full-white" iconLeft={<img src="/wpp.png" width={24} alt="Whatsapp" />}>Contatar</Button>
+                <Button onClick={() => handleDetalhes(request)} variant="full-white" iconLeft={<Eye size={20} />}>Detalhes</Button>
             </div>
         );
+    }
+
+    const handleContatar = (num_telefone: string) => {
+        window.open(`https://wa.me/+55${num_telefone}`, '_blank');
+    }
+
+    const handleDetalhes = (solicitacao: any) => {
+        setSelectedSolicitacao(solicitacao);
+        setIsDetalhesModalOpen(true);
     }
 
     const fetchSolicitacoes = async () => {
@@ -27,17 +39,23 @@ export default function SolicitacoesPage() {
 
         const requests = result.data.requests;
 
+        console.log(requests);
+
         const completeRequests = requests.map((request: any) => {
+            const solicitado_em = getDateString(request.criado_em, 'DD/MM/YYYY HH:mm');
+            const data_chegada = getDateString(request.data_chegada);
+            const data_saida = getDateString(request.data_saida);
+
             return {
                 ...request,
-                acao: acoes(request.id),
+                acao: acoes({...request, data_chegada, data_saida}),
                 criado_em: 
                     <span className={styles.criadoEm}>
                         {request.visualizada === false ? <div className={styles.seenIcon}></div> : null}
-                        {getDateString(request.criado_em, 'DD/MM/YYYY HH:mm')}
+                        {solicitado_em}
                     </span>,
-                data_chegada: getDateString(request.data_chegada),
-                data_saida: getDateString(request.data_saida),
+                data_chegada: data_chegada,
+                data_saida: data_saida,
             }
         });
 
@@ -75,6 +93,45 @@ export default function SolicitacoesPage() {
                 itemsPerPage={9}
             />
         </Card>
+
+        <Modal
+        title="Solicitação de hospedagem"
+        onClose={() => setIsDetalhesModalOpen(false)}
+        isOpen={isDetalhesModalOpen}
+        buttons={
+            <>
+                <Button variant="full-white" style={{color: 'var(--color-error)', borderColor: 'var(--color-error)'}} onClick={() => setIsDetalhesModalOpen(false)}>Fechar</Button>
+                <Button variant="full" iconLeft={<img src="/wpp.png" width={24} alt="Whatsapp" />} onClick={() => handleContatar(selectedSolicitacao.num_telefone)}>Entrar em contato</Button>
+            </>
+        }
+        >
+            <div className={styles.detalhesModalContent}>
+                <div className={styles.part}>
+                    <span className={styles.partTitle}>Nome</span>
+                    <span className={styles.partValue}>{selectedSolicitacao?.nome}</span>
+                </div>
+                <div className={styles.part}>
+                    <span className={styles.partTitle}>Número de telefone</span>
+                    <span className={styles.partValue}>{selectedSolicitacao?.num_telefone}</span>
+                </div>
+                <div className={styles.part}>
+                    <span className={styles.partTitle}>E-mail</span>
+                    <span className={styles.partValue}>{selectedSolicitacao?.email}</span>
+                </div>
+                <div className={styles.part}>
+                    <span className={styles.partTitle}>Data de início</span>
+                    <span className={styles.partValue}>{selectedSolicitacao?.data_chegada}</span>
+                </div>
+                <div className={styles.part}>
+                    <span className={styles.partTitle}>Data de fim</span>
+                    <span className={styles.partValue}>{selectedSolicitacao?.data_saida}</span>
+                </div>
+                <div className={styles.part}>
+                    <span className={styles.partTitle}>Quantidade de pessoas</span>
+                    <span className={styles.partValue}>{selectedSolicitacao?.num_pessoas}</span>
+                </div>
+            </div>
+        </Modal>
         </div>
     );
 }
