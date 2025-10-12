ba5ef6c02c84b19df0abde91c2ceff5bc53fc55d
gabrielcoffee <cafegabriel1@gmail.com>
2025-10-11 23:18:32 -0300

    feat: responsiveness for hospedagem page on desktop

--- src/app/(protected)/hospedagem/page.tsx
+++ src/app/(protected)/hospedagem/page.tsx
@@ -4,6 +4,8 @@ import { PageTitle } from "@/components/home/PageTitle";
 import { Bed } from "lucide-react";
 import { Button } from "@/components/ui/Button";
 import styles from "./page.module.css";
+import Card from "@/components/desktop/Card";
+import CardHeader from "@/components/desktop/CardHeader";
 
 export default function HospedagemPage() {
 
@@ -12,22 +14,54 @@ export default function HospedagemPage() {
     }
 
     return (
-        <div className={styles.container}>
-                <div className={styles.section}>
-                    <PageTitle
-                        icon={<Bed/>}
-                        title="Hospedagem"
-                        text={
-                            <span>
-                                Você pode fazer solicitações de hospedagem para seus convidados. Posteriormente, confirmaremos a disponibilidade.
-                            </span>
-                        }
-                    />
+        <>
+        <div className={styles.mobileContainer}>
+            <div className={styles.section}>
 
-                    <Button onClick={handleExternalLink} variant="full" iconLeft={<Bed/>}>
-                        Solicitar hospedagem
-                    </Button>
-                </div>
+                <PageTitle
+                    icon={<Bed/>}
+                    title="Hospedagem"
+                    text={
+                        <span>
+                            Você pode fazer solicitações de hospedagem para seus convidados. Posteriormente, confirmaremos a disponibilidade.
+                        </span>
+                    }
+                />
+
+                <Button onClick={handleExternalLink} variant="full" iconLeft={<Bed/>}>
+                    Solicitar hospedagem
+                </Button>
+
+            </div>
         </div>
+
+        <div className={styles.desktopContainer}>
+            <Card>
+                <CardHeader title="Hospedagem" breadcrumb={["Início", "Hospedagem"]} />
+
+                <Card>
+                    <div className={styles.section}>
+                        <div className={styles.sectionTitle}>
+                            <PageTitle
+                                icon={<Bed/>}
+                                title="Hospedagem"
+                                text={
+                                    <span>
+                                        Você pode fazer solicitações de hospedagem para seus convidados. Posteriormente, confirmaremos a disponibilidade.
+                                    </span>
+                                }
+                            />
+                        </div>
+
+                        <div className={styles.sectionButtons}>
+                            <Button onClick={handleExternalLink} variant="full" iconLeft={<Bed/>}>
+                                Solicitar hospedagem
+                            </Button>
+                        </div>
+                    </div>
+                </Card>
+            </Card>
+        </div>
+        </>
     )
 }
