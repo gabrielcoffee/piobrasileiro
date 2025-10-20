b4172be8ff4211fe13ea5dc8dbbf5ca5c22f51b5
gabrielcoffee <cafegabriel1@gmail.com>
2025-10-20 10:06:43 -0300

    feat: admin calendar for meals responsive for mobile

--- src/app/admin/(admin-protected)/(refeicoes)/calendario/page.tsx
+++ src/app/admin/(admin-protected)/(refeicoes)/calendario/page.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import Card from '@/components/desktop/Card';
 import CardHeader from '@/components/desktop/CardHeader';
 import { Calendar } from '@/components/admin/Calendar';
+import MobileTitle from '@/components/admin/MobileTitle';
 
 export default function CalendarioPage() {
-    return (
-        <div className={styles.container}>
-            <Card>
-                <CardHeader title="Calendário de refeições" breadcrumb={["Início", "Refeições", "Calendário"]} />
 
-                <Calendar/>
-            </Card>
-        </div>
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(window.innerWidth < 768);
+    }, [window.innerWidth]);
+
+    return (
+        <>
+
+        {!isMobile ? (
+
+            <div className={styles.container}>
+                <Card>
+                    <CardHeader title="Calendário de refeições" breadcrumb={["Início", "Refeições", "Calendário"]} />
+
+                    <Calendar/>
+                </Card>
+            </div>
+
+
+
+        ) : ( /* MOBILE */
+
+
+
+            <div className={styles.mobileContainer}>
+                <MobileTitle title="Calendário de refeições" />
+                <Calendar />
+            </div>
+        )}
+
+        </>
     );
 }
--- src/components/admin/Calendar.tsx
+++ src/components/admin/Calendar.tsx
@@ -5,6 +5,7 @@ import styles from './styles/Calendar.module.css';
 import { useState, useEffect } from 'react';
 import { Button } from '../ui/Button';
 import { queryApi } from '@/lib/utils';
+import SaveFooterAdmin from './SaveFooterAdmin';
 
 export function Calendar() {
     const [currentMonth, setCurrentMonth] = useState(new Date());
@@ -139,13 +140,13 @@ export function Calendar() {
                         <div className={styles.headerContainer}>
                             <div className={styles.monthSelector}>
                                 <button className={styles.leftMonthArrow} onClick={() => moveMonth(-1)}>
-                                    <ChevronLeft size={32} />
+                                    <ChevronLeft className={styles.leftIcon}  />
                                 </button>
                                 <span className={styles.month}>
                                     {new Date(currentMonth.getFullYear(), currentMonth.getMonth()).toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' }).charAt(0).toUpperCase() + new Date(currentMonth.getFullYear(), currentMonth.getMonth()).toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' }).slice(1)}
                                 </span>
                                 <button className={styles.rightMonthArrow} onClick={() => moveMonth(1)}>
-                                    <ChevronRight size={32} />
+                                    <ChevronRight  className={styles.rightIcon}/>
                                 </button>
                             </div>
 
@@ -159,46 +160,68 @@ export function Calendar() {
                             </div>
                         </div>
 
-                        {/* Week Days */}
-                        <div className={styles.weekDaysContainer}>
-                            <div className={styles.weekDay}>SEGUNDA</div>
-                            <div className={styles.weekDay}>TERÇA</div>
-                            <div className={styles.weekDay}>QUARTA</div>
-                            <div className={styles.weekDay}>QUINTA</div>
-                            <div className={styles.weekDay}>SEXTA</div>
-                            <div className={styles.weekDay}>SÁBADO</div>
-                            <div className={styles.weekDay}>DOMINGO</div>
-                        </div>
 
-                        {/* Days */}
-                        <div className={styles.daysContainer}>
-                            {monthDates.map((date, index) => (
-                                <div
-                                    key={`right-${index}`}
-                                    className={`
-                                        ${styles.day}
-                                    ${
-                                        !isInCurrentMonth(date) 
-                                            ? styles.adjacentMonth 
-                                            : ''
-                                    } ${
-                                        blockedDates.includes(date.toISOString().split('T')[0]) ? styles.blockedDate : ''
-                                    } ${
-                                        selectedToBlockDates.includes(date.toISOString().split('T')[0]) ? styles.selectedToBlockDate : ''
-                                    } ${
-                                        selectedDates.includes(date.toISOString().split('T')[0]) ? styles.selectedDate : ''
-                                    } ${
-                                        date.toISOString().split('T')[0] < new Date().toISOString().split('T')[0] ? styles.disabledDate : ''
-                                    }
-                                    `}
-                                    onClick={() => handleDateClick(date.toISOString().split('T')[0])}
-                                >
-                                    {date.getDate()}
-                                </div>
-                            ))}
+                        <div className={styles.calendarMobile}> 
+                            {/* Week Days */}
+                            <div className={styles.weekDaysContainer}>
+                                <div className={styles.weekDay}>SEGUNDA</div>
+                                <div className={styles.weekDay}>TERÇA</div>
+                                <div className={styles.weekDay}>QUARTA</div>
+                                <div className={styles.weekDay}>QUINTA</div>
+                                <div className={styles.weekDay}>SEXTA</div>
+                                <div className={styles.weekDay}>SÁBADO</div>
+                                <div className={styles.weekDay}>DOMINGO</div>
+                            </div>
+
+                            <div className={styles.weekDaysContainerMobile}>
+                                <div className={styles.weekDay}>SEG</div>
+                                <div className={styles.weekDay}>TER</div>
+                                <div className={styles.weekDay}>QUA</div>
+                                <div className={styles.weekDay}>QUI</div>
+                                <div translate='no' className={styles.weekDay}>SEX</div>
+                                <div className={styles.weekDay}>SÁB</div>
+                                <div className={styles.weekDay}>DOM</div>
+                            </div>
+
+                            {/* Days */}
+                            <div className={styles.daysContainer}>
+                                {monthDates.map((date, index) => (
+                                    <div
+                                        key={`right-${index}`}
+                                        className={`
+                                            ${styles.day}
+                                        ${
+                                            !isInCurrentMonth(date) 
+                                                ? styles.adjacentMonth 
+                                                : ''
+                                        } ${
+                                            blockedDates.includes(date.toISOString().split('T')[0]) ? styles.blockedDate : ''
+                                        } ${
+                                            selectedToBlockDates.includes(date.toISOString().split('T')[0]) ? styles.selectedToBlockDate : ''
+                                        } ${
+                                            selectedDates.includes(date.toISOString().split('T')[0]) ? styles.selectedDate : ''
+                                        } ${
+                                            date.toISOString().split('T')[0] < new Date().toISOString().split('T')[0] ? styles.disabledDate : ''
+                                        }
+                                        `}
+                                        onClick={() => handleDateClick(date.toISOString().split('T')[0])}
+                                    >
+                                        {date.getDate()}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className={styles.blockDatesContainerMobile}>
+                <Button available={checkAvailableUnblockDates()} style={{ color: 'var(--color-primary)'}} variant="full-white" iconLeft={<Unlock />} onClick={checkAvailableUnblockDates() ? handleSaveUnblockDates : undefined}>
+                    Liberar datas
+                </Button>
+                <Button available={checkAvailableBlockDates()} style={{ color: 'var(--color-error)', borderColor: 'var(--color-error)' }} variant="full-white" iconLeft={<Lock />} onClick={handleBlockDates}>
+                    Bloquear datas
+                </Button>
             </div>
 
             <div className={styles.confirmContainer}>
@@ -206,6 +229,8 @@ export function Calendar() {
                     Salvar Bloqueios
                 </Button>
             </div>
+
+            <SaveFooterAdmin iconLeft={<Check size={20} />} buttonText="Salvar" executeFunction={handleSaveBlockDates} />
         </div>
     );
 } 
